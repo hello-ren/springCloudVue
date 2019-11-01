f6fdb095aaef5209d86d01fc5e52bac0fedbfa6d
ren-hello <927520114@qq.com>
2019-11-01 11:46:38 +0800

    yao

--- eureka-server-cluster/src/main/java/com/day/eurekaservercluster/EurekaServerClusterApplication.java
+++ eureka-server-cluster/src/main/java/com/day/eurekaservercluster/EurekaServerClusterApplication.java
@@ -1,11 +1,14 @@
 package com.day.eurekaservercluster;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class EurekaServerClusterApplication {
 
 	public static void main(String[] args) {
