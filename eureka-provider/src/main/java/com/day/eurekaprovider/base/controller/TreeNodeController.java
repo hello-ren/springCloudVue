bf5ae9e1bb359462f102ccdd5ccfedc4fb7e4dd5
yx1999 <yx1610351668@163.com>
2019-11-02 09:47:16 +0800

    yao

--- eureka-provider/src/main/java/com/day/eurekaprovider/base/controller/TreeNodeController.java
+++ eureka-provider/src/main/java/com/day/eurekaprovider/base/controller/TreeNodeController.java
@@ -4,6 +4,7 @@ import com.day.eurekaprovider.base.model.TreeNode;
 import com.day.eurekaprovider.base.service.ITreeNodeService;
 import com.day.eurekaprovider.base.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class TreeNodeController {
     @Autowired
     public ITreeNodeService treeNodeService;
 
+    @CrossOrigin // 注解方式
     @RequestMapping("list")
     public JsonData list(TreeNode treeNode){
         List<TreeNode> treeNodes = treeNodeService.listParent(treeNode);
